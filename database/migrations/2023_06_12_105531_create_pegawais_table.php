ede3fddbea6d4588b5e87b48b5d3bd82930a7ec4
MeGGi <megilangr1@gmail.com>
2023-06-12 10:57:34 +0700

    Model & Migration Pegawai

---
+++ database/migrations/2023_06_12_105531_create_pegawais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_toko');
+            $table->string('nama_pegawai');
+            $table->unsignedBigInteger('id_user');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawais');
+    }
+}
